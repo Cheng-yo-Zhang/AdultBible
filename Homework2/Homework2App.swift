5c5336aee03104e167e3a7349b3084c9186fbfa0
Cheng YU Chang <louis20019028@gmail.com>
2024-10-16 14:51:24 +0800

    Initial Commit

---
+++ Homework2/Homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework2App.swift
+//  Homework2
+//
+//  Created by Louis Chang on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct Homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Homework2Tests/Homework2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Homework2Tests.swift
+//  Homework2Tests
+//
+//  Created by Louis Chang on 2024/10/16.
+//
+
+import Testing
+@testable import Homework2
+
+struct Homework2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
